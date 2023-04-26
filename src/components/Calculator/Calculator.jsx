818a0a99d8159bb09f654b20a67a86b1631df6b0
MarwanDev <marwaneg@aucegypt.edu>
2023-04-26 12:36:59 +0200

    Add a child to the calculator

--- src/components/Calculator/Calculator.jsx
+++ src/components/Calculator/Calculator.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import './Calculator.scss';
 import calculate from '../logic/calculate';
 
-function Calculator() {
+function CalculatorBody() {
   const [total, setTotal] = useState(null);
   const [next, setNext] = useState(null);
   const [operation, setOperation] = useState(null);
@@ -13,37 +13,43 @@ function Calculator() {
     setTotal(output.total);
   };
   return (
-    <div className="calculator">
-      <div className="calculator-container">
-        <h2 className="calc-input">
-          {total}
-          {operation}
-          {next}
-        </h2>
-        <div className="calculator-btns-container">
-          <div className="btn-container">
-            <button type="button" onClick={clickHandler}>AC</button>
-            <button type="button" onClick={clickHandler}>+/-</button>
-            <button type="button" onClick={clickHandler}>%</button>
-            <button type="button" onClick={clickHandler} className="operator">÷</button>
-            <button type="button" onClick={clickHandler}>7</button>
-            <button type="button" onClick={clickHandler}>8</button>
-            <button type="button" onClick={clickHandler}>9</button>
-            <button type="button" onClick={clickHandler} className="operator">x</button>
-            <button type="button" onClick={clickHandler}>4</button>
-            <button type="button" onClick={clickHandler}>5</button>
-            <button type="button" onClick={clickHandler}>6</button>
-            <button type="button" onClick={clickHandler} className="operator">-</button>
-            <button type="button" onClick={clickHandler}>1</button>
-            <button type="button" onClick={clickHandler}>2</button>
-            <button type="button" onClick={clickHandler}>3</button>
-            <button type="button" onClick={clickHandler} className="operator">+</button>
-            <button type="button" onClick={clickHandler} className="zero-btn">0</button>
-            <button type="button" onClick={clickHandler}>.</button>
-            <button type="button" onClick={clickHandler} className="operator">=</button>
-          </div>
+    <div className="calculator-container">
+      <h2 className="calc-output">
+        {total}
+        {operation}
+        {next}
+      </h2>
+      <div className="calculator-btns-container">
+        <div className="btn-container">
+          <button type="button" onClick={clickHandler}>AC</button>
+          <button type="button" onClick={clickHandler}>+/-</button>
+          <button type="button" onClick={clickHandler}>%</button>
+          <button type="button" onClick={clickHandler} className="operator">÷</button>
+          <button type="button" onClick={clickHandler}>7</button>
+          <button type="button" onClick={clickHandler}>8</button>
+          <button type="button" onClick={clickHandler}>9</button>
+          <button type="button" onClick={clickHandler} className="operator">x</button>
+          <button type="button" onClick={clickHandler}>4</button>
+          <button type="button" onClick={clickHandler}>5</button>
+          <button type="button" onClick={clickHandler}>6</button>
+          <button type="button" onClick={clickHandler} className="operator">-</button>
+          <button type="button" onClick={clickHandler}>1</button>
+          <button type="button" onClick={clickHandler}>2</button>
+          <button type="button" onClick={clickHandler}>3</button>
+          <button type="button" onClick={clickHandler} className="operator">+</button>
+          <button type="button" onClick={clickHandler} className="zero-btn">0</button>
+          <button type="button" onClick={clickHandler}>.</button>
+          <button type="button" onClick={clickHandler} className="operator">=</button>
         </div>
       </div>
+    </div>
+  );
+}
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <CalculatorBody />
     </div>
   );
 }
