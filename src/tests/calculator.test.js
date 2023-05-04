c52a5dafeba6c656271fe2df45d84cb45b4474cd
Rushema Anicet <rushemaa@gmail.com>
2023-05-04 12:40:01 +0200

    Add calculator test

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -1,9 +1,9 @@
-// import renderer from 'react-test-renderer';
-// import { Calculator } from '../components';
+import renderer from 'react-test-renderer';
+import { Calculator } from '../components';
 
-// it('renders calculator page correctly', () => {
-//   const tree = renderer
-//     .create(<Calculator />)
-//     .toJSON();
-//   expect(tree).toMatchSnapshot();
-// });
+it('renders calculator page correctly', () => {
+  const tree = renderer
+    .create(<Calculator />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
